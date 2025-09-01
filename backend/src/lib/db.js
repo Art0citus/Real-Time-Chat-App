ba6fe5efd6f856c04da9d3b26a50f03221176925
Art0citus <mritik424@gmail.com>
2025-09-01 23:02:49 +0530

    Added real-time chat project (in progress)

---
+++ backend/src/lib/db.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose';
+export const connectDB = async () => {
+    try {
+        await mongoose .connect(process.env.MONGODB_URI);
+        console.log('MongoDB connected: ${conn.connection.host}');
+    } catch (error) {
+        console.log('MONGODB connection error:', error);
+    }
+};
